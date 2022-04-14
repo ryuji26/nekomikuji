7466f77ed5347b39790ae82fec654c267df2e98c
ryuji <ryujispjp@gmail.com>
2022-04-14 17:06:30 +0900

    UI作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nekomikuji/omikuji_page.dart';
+import 'package:nekomikuji/views/omikuji_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: OmikujiPage(),
     );
   }
--- lib/views/omikuji_view.dart
+++ lib/views/omikuji_view.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
 class OmikujiPage extends StatelessWidget {
+  const OmikujiPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('ねこみくじ'),
+          title: const Text('ねこみくじ'),
         ),
         body: Center(
           child: Column(
             children: [
-              Spacer(),
-              Text('ここに画像を表示'),
+              const Spacer(),
+              Image.asset('images/syougatsu2_omijikuji2.png'),
+              const Text(
+                '本日の運勢は...',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
               ElevatedButton(
                 onPressed: () {
                   // TODO おみくじボタン
                 },
-                child: Text(
+                child: const Text(
                   'おみくじをひく',
                 ),
               ),
-              Spacer(),
+              const Spacer(),
             ],
           ),
         ));
