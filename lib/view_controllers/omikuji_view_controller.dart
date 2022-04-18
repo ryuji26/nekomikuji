653aa9065c397aba8c42f29ea57d0b537882e390
ryuji <ryujispjp@gmail.com>
2022-04-18 22:01:31 +0900

    ランダムにおみくじ結果を表示

--- lib/view_controllers/omikuji_view_controller.dart
+++ lib/view_controllers/omikuji_view_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,7 +9,33 @@ class OmikujiController extends ChangeNotifier {
   var omikujiText = '本日の運勢は...';
 
   void omikujiButtonTapped() {
-    omikujiText = "変更したよ";
+    int randomNumber = Random().nextInt(7);
+    switch (randomNumber) {
+      case 0:
+        omikujiText = '大吉';
+        break;
+      case 1:
+        omikujiText = '中吉';
+        break;
+      case 2:
+        omikujiText = '大凶';
+        break;
+      case 3:
+        omikujiText = '小吉';
+        break;
+      case 4:
+        omikujiText = '吉';
+        break;
+      case 5:
+        omikujiText = '凶';
+        break;
+      case 6:
+        omikujiText = '末吉';
+        break;
+      case 7:
+        omikujiText = '大吉';
+        break;
+    }
     notifyListeners();
   }
 }
